7d21776e335528d0326c1d78f7b8223193584c61
ljk <ljk13572@163.com>
2019-05-13 17:57:01 +0800

    9 回文数

---
+++ 705.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 时间:   2019-5-12 11:29
+# 作者:   ljk
+
+
+if []:
+    print('s')
+else:
+    print('ss')
+
+
---
+++ 9.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 时间:   2019-5-13 17:25
+# 作者:   ljk
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return 'false'
+        a = self.get_wei(x)
+        aa = a//2
+        y = 0
+        for i in range(1,aa+1):
+            y += x%(10**(i))*10**(a-i)
+        xx = (x//(10**(a-aa)))*(10**(a-aa))
+        if y == xx:
+            return 'true'
+        else:
+            return 'false'
+
+
+    def get_wei(self,x):
+        i = 0
+        while x>10**i:
+            i = i+1
+        return i
+
+
+a = Solution()
+print(a.isPalindrome(-121))
